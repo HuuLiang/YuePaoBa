178b37ad385ea5f559487fb40163103949daa0c7
Liang <liang@LiangdeMacBook-Pro.local>
2016-03-22 20:01:27 +0800

    remove homecell detail

--- YuePaoBa/YPBConfig.h
+++ YuePaoBa/YPBConfig.h
@@ -9,14 +9,14 @@
 #ifndef YPBConfig_h
 #define YPBConfig_h
 
-#define YPB_CHANNEL_NO   @"QB_MFW_IOS_F_00000001" //@"QB_MFW_IOS_TEST_0000001" //
+#define YPB_CHANNEL_NO       @"QB_MFW_IOS_F_00000001" //@"QB_MFW_IOS_TEST_0000001" //
 
 #define YPB_REST_APPID       @"QUBA_2003"
 #define YPB_REST_PV          @"100"
 #define YPB_REST_APP_VERSION     ((NSString *)([NSBundle mainBundle].infoDictionary[@"CFBundleShortVersionString"]))
 #define YPB_PAYMENT_RESERVE_DATA [NSString stringWithFormat:@"%@$%@", YPB_REST_APPID, YPB_CHANNEL_NO]
 
-#define YPB_BASE_URL                  @"http://mfw.ihuiyx.com"            //@"http://120.24.252.114:8094"     // @"http://192.168.1.123:8094/" //
+#define YPB_BASE_URL                    @"http://mfw.ihuiyx.com" //@"http://120.24.252.114:8094" // @"http://192.168.1.123:8094/"
 #define YPB_USER_ACTIVATION_URL         @"/mfwcenter/jihuo.json"
 #define YPB_USER_REGISTER_URL           @"/mfwcenter/userCreate.json"
 #define YPB_USER_LIST_URL               @"/mfwcenter/userQuery.json"
