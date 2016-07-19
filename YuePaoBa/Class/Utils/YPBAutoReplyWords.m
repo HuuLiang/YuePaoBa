e8d041042b5e7ac39b564398c1d07e4e6c33560b
Liang <q93867@yeah.net>
2016-07-19 17:21:10 +0800

    change autoreplywords

--- YuePaoBa/Class/Utils/YPBAutoReplyWords.m
+++ YuePaoBa/Class/Utils/YPBAutoReplyWords.m
@@ -17,7 +17,7 @@
         _sharedInstance = [[YPBAutoReplyWords alloc] init];
         
         NSString *plistPath = [[NSBundle mainBundle] pathForResource:@"AutoReplyWords" ofType:@"plist"];
-        _sharedInstance.words = [NSArray arrayWithContentsOfFile:plistPath];
+        _sharedInstance.words = [NSDictionary dictionaryWithContentsOfFile:plistPath][[self isBetweenFromHour:7 toHour:23] ? @"day":@"night"];
     });
     return _sharedInstance;
 }
@@ -39,4 +39,42 @@
     
     return [replyWords objectAtIndex:arc4random_uniform((uint32_t)(replyWords.count-1))];
 }
+
++ (BOOL)isBetweenFromHour:(NSInteger)fromHour toHour:(NSInteger)toHour
+{
+    NSDate *date8 = [self getCustomDateWithHour:8];
+    NSDate *date23 = [self getCustomDateWithHour:23];
+    
+    NSDate *currentDate = [NSDate date];
+    
+    if ([currentDate compare:date8]==NSOrderedDescending && [currentDate compare:date23]==NSOrderedAscending)
+    {
+        NSLog(@"该时间在 %ld:00-%ld:00 之间！",fromHour,toHour);
+        return YES;
+    }
+    return NO;
+}
+
++ (NSDate *)getCustomDateWithHour:(NSInteger)hour
+{
+    //获取当前时间
+    NSDate *currentDate = [NSDate date];
+    NSCalendar *currentCalendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
+    NSDateComponents *currentComps = [[NSDateComponents alloc] init];
+    
+    NSInteger unitFlags = NSYearCalendarUnit | NSMonthCalendarUnit | NSDayCalendarUnit | NSWeekdayCalendarUnit | NSHourCalendarUnit | NSMinuteCalendarUnit | NSSecondCalendarUnit;
+    
+    currentComps = [currentCalendar components:unitFlags fromDate:currentDate];
+    
+    //设置当天的某个点
+    NSDateComponents *resultComps = [[NSDateComponents alloc] init];
+    [resultComps setYear:[currentComps year]];
+    [resultComps setMonth:[currentComps month]];
+    [resultComps setDay:[currentComps day]];
+    [resultComps setHour:hour];
+    
+    NSCalendar *resultCalendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
+    return [resultCalendar dateFromComponents:resultComps];
+}
+
 @end
